510a54db391240e48bf94f36aa5661369bfa3fcd
VeerShahCodes <shahveer1213@gmail.com>
2025-01-19 15:54:41 -0800

    made insert and search node

--- Tries/Program.cs
+++ Tries/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Trie trie = new Trie();
+            trie.Insert("Hello");
+            trie.Insert("Happy");
+            trie.Insert("Apple");
+            trie.Insert("Apple");
+            trie.Insert("App");
+            trie.Insert("Happiness");
+            TrieNode node = trie.SearchNode("Happiness");
+            ;
         }
     }
 }
---
+++ Tries/Trie.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tries
+{
+    public class Trie
+    {
+        public TrieNode Head;
+        public Trie()
+        {
+            Head = new TrieNode(' ');
+        }
+        //public void Clear();
+
+        public void Insert(string word)
+        {
+            TrieNode Current = Head;
+            bool idk = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Current.Children.ContainsKey(word[i]) && idk == true)
+                {
+                    Current = Current.Children[word[i]];
+                }
+                else
+                {
+                    idk = false;
+                    Current.Children.Add(word[i], new TrieNode(word[i]));
+                    Current = Current.Children[word[i]];
+                }
+
+
+            }
+            Current.IsWord = true;
+
+
+        }
+
+        public bool Contains(string word)
+        {
+
+        }
+
+        public TrieNode SearchNode(string word)
+        {
+            TrieNode Current = Head;
+            bool idk = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Current.Children.ContainsKey(word[i]) && idk == true)
+                {
+                    Current = Current.Children[word[i]];
+                }
+                else
+                {
+                    return null;
+                }
+
+
+
+
+            }
+            return Current;
+
+        }
+    }
+
+        //public List<string> GetAllMatchingPrefix(string prefix);
+
+        //public bool Remove(string prefix);
+    
+}
