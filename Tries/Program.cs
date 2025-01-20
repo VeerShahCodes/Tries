5f8c4ed2207c771b2e36003849ecb5ee0b85a450
VeerShahCodes <shahveer1213@gmail.com>
2025-01-20 13:50:58 -0800

    made it tell definition and if u got wrong give corrections

--- Tries/Program.cs
+++ Tries/Program.cs
@@ -17,13 +17,39 @@ namespace Tries
                 {
 
                     string word = line.Substring(3, index - 4);
-                    Console.WriteLine(word);
+                    //Console.WriteLine(word);
                     trie.Insert(word);
                     ;
                 }
             }
+            
 
-            Console.WriteLine(trie.Contains("asdf;lkj"));
+
+            Console.WriteLine("Type something and i will give definition:");
+            string prefix = Console.ReadLine();
+            List<string> words = trie.GetAllMatchingPrefix(prefix);
+            if (trie.Contains(prefix))
+            {
+                foreach(string line in File.ReadLines(filePath))
+                {
+                    int index = line.IndexOf(":");
+
+                    if (line.Contains(prefix))
+                    {
+                        Console.WriteLine(line.Substring(index + 2), line.Length - 2);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Did you mean:");
+                foreach (string word in words)
+                {
+                    Console.WriteLine(" " + word);
+                }
+            }
+
+
            // List<string> words = trie.GetAllMatchingPrefix("");
             //foreach(string word in words) { Console.WriteLine(word); }
             ;
